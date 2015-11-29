a2079631e9cc1050352ab86c66ba99abbc4b57ec
Dasha Badanina <b.dasha.ya@gmail.com>
2015-11-30 01:53:57 +0500

    newmessage

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -5,9 +5,11 @@ Server::Application.routes.draw do
   	end
   end
   resources :sessions, only: [:new, :create, :destroy]
+  resources :messages, only: [:create, :destroy]
   root  'static_pages#home'
   match '/signup',     to: 'users#new',            via: 'get'
   match '/signin',  	 to: 'sessions#new',         via: 'get'
   match '/signout',		 to: 'sessions#destroy',     via: 'delete'
   match '/chat', 		   to: 'users#sendMessage',    via: 'get'
+  match '/message',      to: 'messages#message', via: 'get'
 end
