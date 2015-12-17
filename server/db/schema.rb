0e4dc1901193fd685157f5de388f95dd887c5f7d
Dasha Badanina <b.dasha.ya@gmail.com>
2015-12-17 23:35:22 +0500

    status_add

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151203191857) do
+ActiveRecord::Schema.define(version: 20151217171544) do
 
   create_table "conversations", force: true do |t|
     t.integer  "sender_id"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20151203191857) do
     t.datetime "updated_at"
     t.string   "password_digest"
     t.string   "remember_token"
+    t.string   "status"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
