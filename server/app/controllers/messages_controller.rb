b1ee20d8ae2114ec3a2d4720799e2c4615933a69
Dasha Badanina <b.dasha.ya@gmail.com>
2015-12-25 02:23:09 +0500

    emoji

--- server/app/controllers/messages_controller.rb
+++ server/app/controllers/messages_controller.rb
@@ -3,6 +3,7 @@ def create
     @conversation = Conversation.find(params[:conversation_id])
     @message = @conversation.messages.build(message_params)
     @message.user_id = current_user.id
+    @message.body = emojify(@message.body)
     @message.save!    
     @path = conversation_path(@conversation)
 
--- server/db/schema.rb
+++ server/db/schema.rb
@@ -41,8 +41,8 @@ ActiveRecord::Schema.define(version: 20151217223508) do
     t.datetime "updated_at"
     t.string   "password_digest"
     t.string   "remember_token"
-    t.boolean  "admin",           default: false
     t.string   "status"
+    t.boolean  "admin",           default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
